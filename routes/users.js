3da976b966ffcfe40b0754db6d2f07524854bdb5
DESKTOP-B1J9JJ8\prathamesh <prathamesh.netake@gmail.com>
2017-07-03 00:07:08 +0530

    Add login and registration form

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+// Get Login page
+router.get('/login', (req, res) => {
+	res.render("login")
+})
+
+router.post('/login', (req, res) => {
+	console.log(req.body)
+	res.send("Login data recieved");
+})
+
+// Get registe page
+router.get('/register', (req, res) => {
+	res.render("login")
+})
+
+module.exports = router;
