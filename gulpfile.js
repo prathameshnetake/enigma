057c948f7a45b891a8eeda72370d8c34e574ffdb
Prathamesh Netake <prathamesh.netake@zeuslearning.com>
2017-07-11 19:50:55 +0530

    Modified gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-var concat = require('gulp-concat');
+const concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('./prod/scss/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('prod/css'));
-});
-
-gulp.task('css', function() {
-  return gulp.src('./prod/css/*.css')
+    .pipe(gulp.dest('prod/css'))
     .pipe(concat('combine-style.css'))
     .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./prod/**/*.scss', ['sass', 'css']);
+  gulp.watch('./prod/**/*.scss', ['sass']);
 });
